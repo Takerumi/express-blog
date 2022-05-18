ff476861a9c5f54b0597c0906eaf3f138cda9079
Borman <mrgabrielripper@gmail.com>
2022-05-18 13:33:30 +0600

    Add rendering posts from db on home & post pages

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ const postSchema = new mongoose.Schema({
   // create model for posts
   Post = mongoose.model('Post', postSchema)
 
-const posts = [],
-  homeStartingContent =
+const homeStartingContent =
     'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.',
   aboutContent =
     'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.',
@@ -32,9 +31,11 @@ const posts = [],
     'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.'
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, (err, foundPosts) => {
+    res.render('home', {
+      homeStartingContent: homeStartingContent,
+      posts: foundPosts,
+    })
   })
 })
 
@@ -55,16 +56,21 @@ app.post('/compose', (req, res) => {
     title: req.body.postTitle,
     content: req.body.postContent,
   })
-  post.save()
-  res.redirect('/')
+  post.save((err) => {
+    if (!err) {
+      res.redirect('/')
+    }
+  })
 })
 
-app.get('/posts/:title', (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.title)
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title)
-    if (requestedTitle === storedTitle) {
-      res.render('post', { title: post.title, content: post.content })
+app.get('/posts/:postId', (req, res) => {
+  const requestedPostId = req.params.postId
+  Post.findOne({ _id: requestedPostId }, (err, foundPost) => {
+    if (!err) {
+      res.render('post', {
+        title: foundPost.title,
+        content: foundPost.content,
+      })
     }
   })
 })
