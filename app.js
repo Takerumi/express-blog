19bc4f1c77026778b4cf628fa5ec6f668271e237
Borman <mrgabrielripper@gmail.com>
2022-05-18 12:57:44 +0600

    Connect MongoDB to the app, add feature save posts in db

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express'),
+  mongoose = require('mongoose'),
   _ = require('lodash'),
   ejs = require('ejs'),
   app = express(),
-  port = process.env.PORT || 3000
+  port = process.env.PORT || 3000,
+  DB_HOST = process.env.DB_HOST
+
+require('dotenv').config()
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+mongoose.connect(process.env.DB_HOST)
+
+// create schema for posts
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+  }),
+  // create model for posts
+  Post = mongoose.model('Post', postSchema)
 
 const posts = [],
   homeStartingContent =
@@ -11,11 +30,6 @@ const posts = [],
     'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.',
   contactContent =
     'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.'
-
-app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('home', {
@@ -37,11 +51,11 @@ app.get('/compose', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postContent,
-  }
-  posts.push(post)
+  })
+  post.save()
   res.redirect('/')
 })
 
